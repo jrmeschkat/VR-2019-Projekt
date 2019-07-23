80970429f067f8ea554903007bc37711a5cf62d3
Robert Meschkat <robert.meschkat@gmail.com>
2019-07-23 17:55:46 +0200

    first data

---
+++ Unity/Project/Assets/Scripts/TestParticle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestParticle : MonoBehaviour
+{
+    public ParticleSystem particle;
+
+    private float time = 0;
+    private int dir = 1;
+    private ParticleSystem.ShapeModule shape;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shape = particle.shape;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((time += Time.deltaTime) > .1f) {
+            time = 0;
+            float angle = shape.angle;
+            if(angle >= 80 ||angle <= 10) {
+                dir *= -1;
+            }
+
+            shape.angle = shape.angle + 1 * dir;
+            
+        }
+    }
+}
---
+++ Unity/Project/Assets/Scripts/TestRotate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRotate : MonoBehaviour {
+    private Transform front;
+    private Transform flush;
+    private Transform handle;
+
+    private float time = 0;
+    private float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start() {
+        front = transform.Find("front");
+        flush = transform.Find("flush");
+        handle = transform.Find("handle");
+
+        if (front == null || flush == null || handle == null) {
+            throw new System.Exception("Missing child");
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
+        if ((time += Time.deltaTime) > .1f) {
+            time = 0;
+
+            front.transform.Rotate(0, speed, 0, Space.Self);
+            flush.transform.Rotate(0, speed, 0, Space.Self);
+            handle.transform.Rotate(speed, 0, 0, Space.Self);
+        }
+    }
+
+}
