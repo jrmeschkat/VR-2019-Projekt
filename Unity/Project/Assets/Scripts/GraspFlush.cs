d2756a9c5b4bdf560d0d061aa40bd9bec0f488b5
Robert Meschkat <robert.meschkat@gmail.com>
2019-07-30 14:52:57 +0200

    wip

---
+++ Unity/Project/Assets/Scripts/GraspFlush.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Leap.Unity.Interaction;
+
+[RequireComponent(typeof(InteractionBehaviour))]
+public class GraspFlush : MonoBehaviour
+{
+    private InteractionBehaviour interact;
+    private MaterialPropertyBlock mat;
+    private Renderer render;
+
+    void Start()
+    {
+        mat = new MaterialPropertyBlock();
+        mat.SetColor("_Color", Color.red);
+        interact = GetComponent<InteractionBehaviour>();
+        render = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (interact.isGrasped)
+        {
+            render.SetPropertyBlock(mat);
+        }
+        else
+        {
+            render.SetPropertyBlock(null);
+        }
+    }
+}
---
+++ Unity/Project/Assets/Scripts/GraspHandle.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Leap.Unity.Interaction;
+using Leap.Unity;
+
+[RequireComponent(typeof(InteractionBehaviour))]
+public class GraspHandle : MonoBehaviour
+{
+    private InteractionBehaviour interact;
+    private MaterialPropertyBlock mat;
+    private Renderer render;
+
+    void Start()
+    {
+        mat = new MaterialPropertyBlock();
+        mat.SetColor("_Color", Color.red);
+        interact = GetComponent<InteractionBehaviour>();
+        render = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (interact.isGrasped)
+        {
+            render.SetPropertyBlock(mat);
+            var sourceHand = Hands.Get(Chirality.Right);
+            sourceHand = (sourceHand != null ? sourceHand : Hands.Get(Chirality.Left));
+
+            if(sourceHand != null)
+            {
+                
+            }
+        }
+        else
+        {
+            render.SetPropertyBlock(null);
+        }
+    }
+}
